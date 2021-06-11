070a79acea463fc51ea1c379c718925dbfceeda3
Jimmy <10526094+JimmyWhitaker@users.noreply.github.com>
2021-06-11 16:30:01 -0400

    Create download_model.sh
    Created download model script

---
+++ market-sentiment/download_model.sh
@@ -0,0 +1,7 @@
+mkdir -p ./models/finbertTCR2/
+wget --directory-prefix=./models/finbertTCR2/ https://huggingface.co/ProsusAI/finbert/resolve/main/README.md
+wget --directory-prefix=./models/finbertTCR2/ https://huggingface.co/ProsusAI/finbert/resolve/main/config.json
+wget --directory-prefix=./models/finbertTCR2/ https://huggingface.co/ProsusAI/finbert/resolve/main/pytorch_model.bin
+wget --directory-prefix=./models/finbertTCR2/ https://huggingface.co/ProsusAI/finbert/resolve/main/special_tokens_map.json
+wget --directory-prefix=./models/finbertTCR2/ https://huggingface.co/ProsusAI/finbert/resolve/main/tokenizer_config.json
+wget --directory-prefix=./models/finbertTCR2/ https://huggingface.co/ProsusAI/finbert/resolve/main/vocab.txt
