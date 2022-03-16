fdeea54065d9618fa2bd3fbb0be69a77d051172d
Jimmy Whitaker <jimmymwhitaker@gmail.com>
2022-03-16 19:35:35 -0400

    Removed mysql-ingest example

--- mysql-ingest/README.md
+++
@@ -1,5 +0,0 @@
-# MySQL Ingestion 
-
-This example shows how to use `jsonnet` pipeline specs to ingest queried data from a MySQL database into a Pachyderm repo. 
-
-For the full walkthrough see the [demo notebook](SQL%20Ingestion%20Example.ipynb).
--- mysql-ingest/create_sql_ingest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pachctl update pipeline --jsonnet ./sql_ingest_cron.jsonnet \
-	--arg name=myingest \
-	--arg url="mysql://root@mysql:3306/test_db" \
-	--arg query="SELECT * FROM test_data" \
-	--arg cronSpec="@every 30s" \
-	--arg secretName="mysql-creds" \
-	--arg format=json
