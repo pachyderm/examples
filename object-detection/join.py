15323b3bb39ba944f133593c39cb974daf4c5c14
Jimmy Whitaker <jimmymwhitaker@gmail.com>
2022-08-01 17:47:53 -0500

    WIP Added join and wandb model pipeline

--- object-detection/join.py
+++ object-detection/join.py
@@ -3,17 +3,18 @@ import os
 import sys
 import json
 from loguru import logger
+import python_pachyderm
+from time import sleep
+from utils import coco_merge
 
 
 parser = argparse.ArgumentParser(description="Label Studio to Coco conversion")
 parser.add_argument("--input",
                     metavar="DIR",
-                    help="input video or directory of videos",
-                    default="./samples")
+                    help="input directory of annotation files from Label Studio")
 parser.add_argument("--output",
-                    metavar="DIR",
                     default="./output",
-                    help="output directory for extracted frames")
+                    help="output directory for extracted frames or pachyderm repo to egress to (ex. repo@branch:/path/to/dir/)")
 
 
 def load_ls_json(label_path):
@@ -28,12 +29,24 @@ def bbox_pct_to_pxl(x, y, width, height, original_height, original_width):
     pixel_height = height / 100.0 * original_height
     return [int(pixel_x), int(pixel_y), int(pixel_x+pixel_width), int(pixel_y+pixel_height)]
 
+def bbox_pct_to_pxl2(x, y, width, height, original_height, original_width):
+    pixel_x = x / 100.0 * original_width
+    pixel_y = y / 100.0 * original_height
+    pixel_width = width / 100.0 * original_width
+    pixel_height = height / 100.0 * original_height
+    return [int(x), int(y), pixel_width, pixel_height]
+
 def ls_to_coco(ls_data, convert_pfs_name=False):
     # image data
     if convert_pfs_name:
         file_name = ls_data['task']['data']['image'].partition('?d=')[-1]
     else: 
         file_name = ls_data['task']['data']['image']
+        
+    logger.info(
+            "Converting: Label Studio file {}".format(
+                file_name)
+        )
     
     image_data = {'width': ls_data['result'][0]['original_width'],
                  'height': ls_data['result'][0]['original_height'],
@@ -58,7 +71,7 @@ def ls_to_coco(ls_data, convert_pfs_name=False):
         category = r['value']['rectanglelabels'][0]
         category_id = indexed_categories[category]
         
-        bbox = bbox_pct_to_pxl(r['value']['x'], 
+        bbox = bbox_pct_to_pxl2(r['value']['x'], 
                                r['value']['y'],
                                r['value']['width'],
                                r['value']['height'],
@@ -71,6 +84,7 @@ def ls_to_coco(ls_data, convert_pfs_name=False):
                       "category_id": category_id,
                       "segmentation": [],
                       "bbox": bbox,
+                      "area": r['value']['width']*r['value']['height'],
                       "ignore": 0,
                       "iscrowd": 0}
         annotations.append(annotation)
@@ -80,63 +94,67 @@ def ls_to_coco(ls_data, convert_pfs_name=False):
     return coco_data
 
 
-@logger.catch(reraise=True)
-def coco_merge(data_extend: dict, data_add: dict) -> str:
-    """Merge COCO annotation files."""
+def write_to_pachyderm(dataset, output_path):
+    
+    dest_repo = output_path.split('@', 1)[0]
+    dest_branch = output_path.split('@', 1)[1].split('/',1)[0].replace(':', '')
+    dest_path_base = '/'
 
-    output: Dict[str, Any] = {
-        k: data_extend[k] for k in data_extend if k not in ("images", "annotations")
-    }
+    try:  # if directory specificed ('repo@branch:/path/')
+        dest_path_base = os.path.join(dest_path_base, output_path.split('/',1)[1])
+    except IndexError:  # if no directory specificed ('repo@branch')
+        pass
+    
+    client = python_pachyderm.Client.new_in_cluster()
+    with client.commit(dest_repo, dest_branch) as dest_commit:
+        
+        # Copy image files to destination
+        for i in dataset['images']:
+            src_filename = i['file_name']
 
-    output["images"], output["annotations"] = [], []
+            src_repo = src_filename.split('@', 1)[0]
+            src_branch = src_filename.split('@', 1)[1].split('/',1)[0].replace(': ', '')
+            src_path = os.path.join('/',src_filename.split('/',1)[1])
 
-    for i, data in enumerate([data_extend, data_add]):
+            dest_path = os.path.join(dest_path_base, 'images', os.path.basename(src_path))
 
-        logger.info(
-            "Input {}: {} images, {} annotations".format(
-                i + 1, len(data["images"]), len(data["annotations"])
+            logger.info(
+                "Copying: Source image {}, to {}".format(
+                    src_repo + '@' + src_branch + ':' + src_path, dest_repo + '@' + dest_branch + ':' + dest_path)
             )
-        )
 
-        cat_id_map = {}
-        for new_cat in data["categories"]:
-            new_id = None
-            for output_cat in output["categories"]:
-                if new_cat["name"] == output_cat["name"]:
-                    new_id = output_cat["id"]
-                    break
+            
+            with client.commit(src_repo, src_branch) as src_commit:
+                client.copy_file(source_commit=src_commit, 
+                                 source_path=src_path, 
+                                 dest_commit= dest_commit, 
+                                 dest_path=dest_path)
+            sleep(0.2)
+                
+            # Update dataset
+            i['file_name']=dest_path[1:] # Remove leading '/'
 
-            if new_id is not None:
-                cat_id_map[new_cat["id"]] = new_id
-            else:
-                new_cat_id = max(c["id"] for c in output["categories"]) + 1
-                cat_id_map[new_cat["id"]] = new_cat_id
-                new_cat["id"] = new_cat_id
-                output["categories"].append(new_cat)
+        dataset_output_path = os.path.join(dest_path_base, 'annotations/ls_dataset.json')
+        logger.info(
+                "Writing: Coco Dataset file to {}".format(
+                    dest_repo + '@' + dest_branch + ':' + dataset_output_path)
+            )
+        # with client.commit(dest_repo, dest_branch) as c:
+        client.put_file_bytes(dest_commit, dataset_output_path, json.dumps(dataset, indent=2).encode('utf-8'))
+    
 
-        img_id_map = {}
-        for image in data["images"]:
-            n_imgs = len(output["images"])
-            img_id_map[image["id"]] = n_imgs
-            image["id"] = n_imgs
-
-            output["images"].append(image)
-
-        for annotation in data["annotations"]:
-            n_anns = len(output["annotations"])
-            annotation["id"] = n_anns
-            annotation["image_id"] = img_id_map[annotation["image_id"]]
-            annotation["category_id"] = cat_id_map[annotation["category_id"]]
-
-            output["annotations"].append(annotation)
-
-    logger.info(
-        "Result: {} images, {} annotations".format(
-            len(output["images"]), len(output["annotations"])
-        )
-    )
-
-    return output
+def write_output(dataset, output_path):
+    """Write COCO dataset ouput.
+    dataset: Dict of coco dataset values
+    output_path: local path or pachyderm repo path to write ls_dataset.json
+    
+    """
+    if os.path.isdir(output_path):
+        with open(os.path.join(output_path, 'ls_dataset.json'), "w") as f:
+            json.dump(dataset, f, indent=None)
+    else: # coco128@master:/inference_images
+        write_to_pachyderm(dataset, output_path)
+        
 
 
 def main():
@@ -150,9 +168,13 @@ def main():
         for f in os.listdir(args.input):
             label_paths.append(os.path.join(args.input, f))
             
-    
+    # Convert LS files to coco dataset
     coco_dataset = None
     for input_path in label_paths:
+        logger.info(
+            "Reading: Label Studio file {}".format(
+                input_path)
+        )
         ls_data = load_ls_json(input_path)
         coco_data = ls_to_coco(ls_data, convert_pfs_name=True)
         if coco_dataset: 
@@ -160,9 +182,7 @@ def main():
         else:
             coco_dataset = coco_data
     
-    with open(os.path.join(args.output, 'ls_dataset.json'), "w") as f:
-        json.dump(coco_dataset, f, indent=None)
-
+    write_output(coco_dataset, args.output)
         
             
 if __name__ == "__main__":
