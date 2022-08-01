4d81e2991e171786cd236be6f12e1aeebda734f8
Jimmy Whitaker <jimmymwhitaker@gmail.com>
2022-08-01 17:47:53 -0500

    WIP Added join and wandb model pipeline

--- object-detection/train.py
+++ object-detection/train.py
@@ -3,17 +3,52 @@ import flash
 from flash.core.data.utils import download_data
 from flash.image import ObjectDetectionData, ObjectDetector
 from os import path
+import os
+import json
+import tempfile
+
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+
+from utils import coco_merge
 
 parser = argparse.ArgumentParser(description="Train an object detecion model")
 parser.add_argument("--train-dir", metavar="DIR", default="./output", help="input directory")
 parser.add_argument(
     "--output-dir", metavar="DIR", default="./output", help="output directory"
 )
+parser.add_argument(
+        "--wandb-project",
+        type=str,
+        help="For loading the dataset",
+    )
+
+def merge_datasets(train_dir):
+    with open(path.join(train_dir, "coco128/annotations/instances_train2017.json")) as f:
+        dataset = json.load(f)
+        for i in dataset['images']: 
+            i['file_name'] = os.path.join('coco128/images/train2017/', i['file_name'])
+
+    if os.path.exists(path.join(train_dir, 'inference_images/annotations/ls_dataset.json')):
+        with open(path.join(train_dir, 'inference_images/annotations/ls_dataset.json')) as f:
+            infer_dataset = json.load(f)
+            dataset = coco_merge(dataset, infer_dataset)
+            
+    annotations_file = tempfile.NamedTemporaryFile()
+    
+    # Open the file for writing.
+    with open(annotations_file.name, 'w') as f:
+        f.write(json.dumps(dataset))
+    return annotations_file
+    
 
 def create_datamodule(train_dir):
+    
+    ann_file = merge_datasets(train_dir)
+    
     datamodule = ObjectDetectionData.from_coco(
-        train_folder=path.join(train_dir, "coco128/images/train2017/"),
-        train_ann_file=path.join(train_dir, "coco128/annotations/instances_train2017.json"),
+        train_folder=train_dir,
+        train_ann_file=ann_file.name,
         val_split=0.1,
         transform_kwargs={"image_size": 512},
         batch_size=4,
@@ -21,11 +56,12 @@ def create_datamodule(train_dir):
     
     return datamodule
 
-def train_model(datamodule): 
+def train_model(datamodule, logger=None): 
     model = ObjectDetector(head="efficientdet", backbone="d0", num_classes=datamodule.num_classes, image_size=512, labels=datamodule.labels)
-    
+
     # Create the trainer and finetune the model
-    trainer = flash.Trainer(max_epochs=1)
+    trainer = flash.Trainer(max_epochs=5, logger=logger)
+
     trainer.finetune(model, datamodule=datamodule, strategy="freeze")
     
     return trainer
@@ -36,11 +72,17 @@ def save_model(model, model_path):
 def main():
     args = parser.parse_args()
     
+    logger = None
+    if args.wandb_project:
+        logger = WandbLogger(
+            project=args.wandb_project, name=str(os.getenv("PACH_JOB_ID", "Local"))
+        )
+    
     # 1. Load data
     datamodule = create_datamodule(args.train_dir)
     
     # 2. Train model
-    trainer = train_model(datamodule)
+    trainer = train_model(datamodule, logger=logger)
     
     # 3. Save the model!
     save_model(trainer, path.join(args.output_dir, "object_detection_model.pt"))
---
+++ object-detection/utils.py
@@ -0,0 +1,59 @@
+from loguru import logger
+
+@logger.catch(reraise=True)
+def coco_merge(data_extend: dict, data_add: dict) -> str:
+    """Merge COCO annotation files."""
+
+    output: Dict[str, Any] = {
+        k: data_extend[k] for k in data_extend if k not in ("images", "annotations")
+    }
+
+    output["images"], output["annotations"] = [], []
+
+    for i, data in enumerate([data_extend, data_add]):
+
+        logger.info(
+            "Input {}: {} images, {} annotations".format(
+                i + 1, len(data["images"]), len(data["annotations"])
+            )
+        )
+
+        cat_id_map = {}
+        for new_cat in data["categories"]:
+            new_id = None
+            for output_cat in output["categories"]:
+                if new_cat["name"] == output_cat["name"]:
+                    new_id = output_cat["id"]
+                    break
+
+            if new_id is not None:
+                cat_id_map[new_cat["id"]] = new_id
+            else:
+                new_cat_id = max(c["id"] for c in output["categories"]) + 1
+                cat_id_map[new_cat["id"]] = new_cat_id
+                new_cat["id"] = new_cat_id
+                output["categories"].append(new_cat)
+
+        img_id_map = {}
+        for image in data["images"]:
+            n_imgs = len(output["images"])
+            img_id_map[image["id"]] = n_imgs
+            image["id"] = n_imgs
+
+            output["images"].append(image)
+
+        for annotation in data["annotations"]:
+            n_anns = len(output["annotations"])
+            annotation["id"] = n_anns
+            annotation["image_id"] = img_id_map[annotation["image_id"]]
+            annotation["category_id"] = cat_id_map[annotation["category_id"]]
+
+            output["annotations"].append(annotation)
+
+    logger.info(
+        "Result: {} images, {} annotations".format(
+            len(output["images"]), len(output["annotations"])
+        )
+    )
+
+    return output
